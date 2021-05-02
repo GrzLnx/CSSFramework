6ca9ddf5cd1619adaf8f3b1f0e238be0ac6a9cd6
GrzLnx <53223526+GrzLnx@users.noreply.github.com>
2021-05-02 19:47:45 +0200

    updates

--- js/forms.js
+++ js/forms.js
@@ -1,18 +1,19 @@
 function sendForm( f_formId ) {
     const form = document.getElementById( f_formId )
+    event.preventDefault()
 
     const CheckFormClass = new CheckForm( form )
 
-    return CheckFormClass.checkFormFields()
-
-    // form.submit()
+    if( CheckFormClass.checkFormFields() === true ) {
+        form.submit()
+    }
 }
 
 class CheckForm {
 
     _form = HTMLFormElement
     _checkForm = Boolean
-    _formResult = Boolean
+    _formResult = true
 
     constructor( form ) {
         this.form = form
@@ -52,12 +53,18 @@ class CheckForm {
 
     checkFormFields() {
         const fields = this.form.querySelectorAll( '.form__field[data-js-form-field-check]' )
-        Array.from( fields ).forEach( this.checkFormField )
-        return false
+        for( let field of fields ) {
+            this.checkFormField( field )
+        }
+        return this.formResult
     }
 
     checkFormField( field ) {
         const CheckFormFieldClass = new CheckFormField( field )
+        const result = CheckFormFieldClass.checkField()
+        if( result !== true ) {
+            this.formResult = false
+        }
     }
 
 }
@@ -65,6 +72,7 @@ class CheckForm {
 class CheckFormField {
 
     _field = HTMLElement
+    _fieldValue = ''
     _fieldType = String
     _fieldTypes = [
         { 'name': 'text', 'class': 'form__field-text' },
@@ -74,6 +82,7 @@ class CheckFormField {
     ]
     _fieldCheckTypes = []
     _fieldStatusText = String
+    _fieldStatusErrors = ''
     _fieldResult = Boolean
 
     constructor( field ) {
@@ -81,8 +90,8 @@ class CheckFormField {
 
         this.setFieldType()
         this.setFieldCheckTypes()
+        this.setFieldValue()
 
-        console.log( this )
     }
 
     get field() {
@@ -90,6 +99,13 @@ class CheckFormField {
     }
     set field(value) {
         this._field = value
+    }
+
+    get fieldValue() {
+        return this._fieldValue
+    }
+    set fieldValue(value) {
+        this._fieldValue = value
     }
 
     get fieldType() {
@@ -120,6 +136,13 @@ class CheckFormField {
         this._fieldStatusText = value
     }
 
+    get fieldStatusErrors() {
+        return this._fieldStatusErrors
+    }
+    set fieldStatusErrors(value) {
+        this._fieldStatusErrors = value
+    }
+
     get fieldResult() {
         return this._fieldResult
     }
@@ -139,6 +162,60 @@ class CheckFormField {
 
     }
 
+    setFieldValue() {
+
+        switch( this.fieldType ) {
+            case 'text':
+                this.fieldValue = this.field.getElementsByTagName( 'input' )[0].value
+                break
+            case 'radios':
+                let radioFields = this.field.getElementsByTagName( 'input' )
+                for( let radioField of radioFields ) {
+                    if( radioField.checked === true ) {
+                        this.fieldValue = radioField.value
+                    }
+                }
+                break
+            case 'checkboxes':
+                let fieldValues = []
+                let checkboxFields = this.field.getElementsByTagName( 'input' )
+                for( let checkboxField of checkboxFields ) {
+                    if( checkboxField.checked === true ) {
+                        fieldValues.push( { 'field': checkboxField, 'value': checkboxField.value } )
+                    }
+                }
+                this.fieldValue = fieldValues
+                break
+            case 'checkbox':
+                let checkboxField = this.field.getElementsByTagName( 'input' )[0]
+                if( checkboxField.checked === true ) {
+                    this.fieldValue = checkboxField.value
+                }
+                break
+        }
+
+    }
+
+    setFormFieldLabelHeight() {
+
+        switch( this.fieldType ) {
+            case 'text':
+                const formFieldLabelHeight = this.field.getElementsByClassName( 'form-field-label__text' )[0].clientHeight
+                this.field.style.setProperty( '--form-label--height', formFieldLabelHeight )
+                break
+            case 'radios':
+                this.field.style.setProperty( '--form-label--height', 0 )
+                break
+            case 'checkboxes':
+                this.field.style.setProperty( '--form-label--height', 0 )
+                break
+            case 'checkbox':
+                this.field.style.setProperty( '--form-label--height', 0 )
+                break
+        }
+
+    }
+
     setFieldCheckTypes() {
 
         const checks = this.field.getAttribute( 'data-js-form-field-check' ).split( ';' )
@@ -155,6 +232,103 @@ class CheckFormField {
         let fieldCheckTypes = this.fieldCheckTypes
         fieldCheckTypes.push( { 'name': fieldCheckTypeName, 'value': fieldCheckTypeValue } )
         this.fieldCheckTypes = fieldCheckTypes
+
+    }
+
+    checkField() {
+        this.fieldResult = true
+
+        for( let check of this.fieldCheckTypes ) {
+            switch( check.name ) {
+                case 'required':
+                    this.validateFieldRequired()
+                    break
+                case 'format':
+                    this.validateFieldFormat( check.value )
+                    break
+                case 'min-length':
+                    this.validateFieldMinLength( check.value )
+                    break
+                case 'max-length':
+                    this.validateFieldMaxLength( check.value )
+                    break
+                case 'password-confirm':
+                    this.validateFieldPasswordConfirm( check.value )
+                    break
+            }
+        }
+
+        this.setFieldStatusText()
+        return this.fieldResult
+    }
+
+    validateFieldRequired() {
+
+        if( this.fieldValue.length === 0 ) {
+            this.fieldResult = false
+            this.fieldStatusErrors += '<br />- Dit veld is verplicht.'
+            return false
+        }
+        return true
+
+    }
+    validateFieldFormat( checkValue ) {
+        switch( checkValue ) {
+            case 'email':
+                return this.validateFieldFormatEmail()
+            case 'numbers':
+                return this.validateFieldFormatNumbers()
+            default:
+                return true
+        }
+    }
+    validateFieldFormatEmail() {
+        if( !/(.*[@].*\.)/.test( this.fieldValue ) ) {
+            this.fieldResult = false
+            this.fieldStatusErrors += '<br />- Dit veld moet een geldig e-mailadres bevatten.'
+            return false
+        }
+        return true
+    }
+    validateFieldFormatNumbers() {
+        if( !/(^\d*$)/.test( this.fieldValue ) ) {
+            this.fieldResult = false
+            this.fieldStatusErrors += '<br />- Dit veld mag enkel cijfers bevatten.'
+            return false
+        }
+        return true
+    }
+    validateFieldMinLength( checkValue ) {
+        if( this.fieldValue.length < checkValue ) {
+            this.fieldResult = false
+            this.fieldStatusErrors += '<br />- Dit veld moet minimaal ' + checkValue + ' tekens bevatten.'
+            return false
+        }
+        return true
+    }
+    validateFieldMaxLength( checkValue ) {
+        if( this.fieldValue.length > checkValue ) {
+            this.fieldResult = false
+            this.fieldStatusErrors += '<br />- Dit veld mag maximaal ' + checkValue + ' tekens bevatten.'
+            return false
+        }
+        return true
+    }
+    validateFieldPasswordConfirm( checkValue ) {
+
+    }
+
+    setFieldStatusText() {
+
+        if( this.fieldResult === true ) {
+            this.fieldStatusText = '<strong>Dit veld is correct ingevuld</strong>'
+        } else {
+            this.fieldStatusText = '<strong>Dit veld bevat de volgende errors: </strong>' + this.fieldStatusErrors
+        }
+
+        this.setFormFieldLabelHeight()
+        this.field.setAttribute( 'data-js-form-field-status', this.fieldResult )
+        this.field.getElementsByClassName( 'form-field-status__tooltip' )[0].innerHTML = this.fieldStatusText
 
     }
 
